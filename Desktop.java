7f178c71f951b715fdc8f4abdabf3cad8d99596a
gangaimurali <84244626+gangaimurali@users.noreply.github.com>
2021-05-22 21:03:16 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,17 @@
+package org.system;
+
+public class Desktop extends Computer{
+	public void desktopSize() {
+		System.out.println("15 inch");
+
+	}
+
+	public static void main(String[] args) {
+		Desktop d=new Desktop();
+		d.computerModel();
+		d.desktopSize();
+		
+
+	}
+
+}
